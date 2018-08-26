4720af815ba0b2edda899ab7763ac3867c871217
JDES9993 <31172390+JDES9993@users.noreply.github.com>
2018-08-25 23:18:24 -0500

    Boton de settings agregado Tal vez

--- app/src/main/java/com/example/ricardo/hack_2018/MainActivity.java
+++ app/src/main/java/com/example/ricardo/hack_2018/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ricardo.hack_2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -11,8 +13,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // #4B120E, #E6D199, #DFD88A, #C7E4E7
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi=getMenuInflater();
+        mi.inflate(R.menu.action_bar1, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
