f58124552c42d657ab60a6df51c8c324f1043cd7
JDES9993 <31172390+JDES9993@users.noreply.github.com>
2018-08-25 23:29:02 -0500

    YA

--- app/src/main/java/com/example/ricardo/hack_2018/MainActivity.java
+++ app/src/main/java/com/example/ricardo/hack_2018/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.ricardo.hack_2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -24,5 +26,9 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mi=getMenuInflater();
         mi.inflate(R.menu.action_bar1, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    public void goToTime(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
